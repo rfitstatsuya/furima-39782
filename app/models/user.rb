42bf8cce0c1b18c8bdc5b90867e2dd9c3a576713
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-12 16:56:08 +0900

    userモデル　バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :birthday
+  end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
