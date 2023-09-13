020d500b790152f29577854b20ebdfebe5ac0398
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-13 16:39:38 +0900

    itemモデル　作成・編集

---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :derivery_cost
+  belongs_to :prefecture
+  belongs_to :sending_date
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price, format: {with: /\A [0-9]+ \z/ }
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :derivery_cost_id
+    validates :prefecture_id
+    validates :sending_date_id
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_912_070_534) do
-  create_table 'users', charset: 'utf8', force: :cascade do |t|
-    t.string 'nickname', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.string 'first_name_reading', null: false
-    t.string 'last_name_reading', null: false
-    t.date 'birthday', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_060709) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "derivery_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "sending_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_reading", null: false
+    t.string "last_name_reading", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "items", "users"
 end
