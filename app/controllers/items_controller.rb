f9970a8a4413fcad97ad8db843b7b87d10d3943a
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-14 09:29:24 +0900

    コントローラー・ビュー　編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,31 @@
 class ItemsController < ApplicationController
-  def index; end
+  before_action :move_to_index, except: [:index]
+  
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :derivery_cost_id, :prefecture_id, :sending_date_id, :price).merge(user_id: current_user.id)
+  end
 end
