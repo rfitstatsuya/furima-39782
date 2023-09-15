d0534bc9b76f8c4c7b2d7854332431c37aa34cb4
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-15 16:35:05 +0900

    商品一覧表示　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :move_to_sign_in, only: [:new]
-  
+
   def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -20,12 +21,13 @@ class ItemsController < ApplicationController
   private
 
   def move_to_sign_in
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
   end
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :derivery_cost_id, :prefecture_id, :sending_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :derivery_cost_id,
+                                 :prefecture_id, :sending_date_id, :price).merge(user_id: current_user.id)
   end
 end
