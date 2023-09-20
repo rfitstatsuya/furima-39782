5b8fe4d1db90219dc06fe48970d64dac88ca62ff
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-20 17:23:23 +0900

    モデル（クレジットカード決済）　実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :house_number, :building_name,
-                :telephone_number, :order_id
+                :telephone_number, :order_id, :token
 
   with_options presence: true do
     validates :item_id
@@ -10,6 +10,7 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
