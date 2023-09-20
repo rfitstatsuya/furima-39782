1407ffc26529040e15692135edfcf16c44193836
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-09-20 18:08:14 +0900

    Rubocop 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :orders
-  
+
   with_options presence: true do
     validates :nickname
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
